9caa9f5e6036d8c4f8f6b0fcab595b34aa88a83c
fuerbringer <severin@protonmail.ch>
2019-07-21 12:33:57 +0200

    Implement LET command for the basicinterpreter problem.

--- basicinterpreter/basic.php
+++ basicinterpreter/basic.php
@@ -1,10 +1,20 @@
 <?php
+//------------------------------------------------------------------------------
+// NOTE:  This BASIC interpreter is by no means complete or fully featured.
+//        It is specifically designed to pass the open.kattis.com challenge
+//        "basicinterpreter" and misses features such as error handling and
+//        proper arithmetic.
+
+
+//------------------------------------------------------------------------------
+// Globals
 
 $instructions = [];
 $memory = [];
 
-//------------------------------------------------------------------------------
 
+//------------------------------------------------------------------------------
+// Classes
 class Command
 {  
   private $command;
@@ -12,7 +22,7 @@ class Command
 
   public function __construct($command, $parameters) {
     $this->command = $command;
-    $this->parameters = $parameters;
+    $this->setParameters($parameters);
   }
 
   public function setCommand($command) {
@@ -20,6 +30,9 @@ class Command
   }
 
   public function setParameters($parameters) {
+    for($i = 0; $i < count($parameters); $i++) {
+      $parameters[$i] = str_replace("\n", '', $parameters[$i]);
+    }
     $this->parameters = $parameters;
   }
 
@@ -49,6 +62,8 @@ class Instruction extends Command {
   }
 }
 
+//------------------------------------------------------------------------------
+// Functions
 function cmp($a, $b) {
   if($a->line() == $b->line()) {
     return 0;
@@ -56,6 +71,9 @@ function cmp($a, $b) {
   return ($a->line() < $b->line()) ? -1 : 1;
 }
 
+
+//------------------------------------------------------------------------------
+// Figure out what array-index $line has in instruction list $a.
 function lineToArrayIndex($a, $line) {
   for($i = 0; $i < size($instructions); $i++) {
     $ins = $instructions[$i];
@@ -65,32 +83,129 @@ function lineToArrayIndex($a, $line) {
   return -1;
 }
 
-//------------------------------------------------------------------------------
+function to32BitSigned($n) {
+  // Enforce 32-bit signed arithmetic as required by the problem description.
+  // That's what I get for solving this in PHP..
+  $mv = $n & 0xFFFFFFFF;
+  if ($mv & 0x80000000) {
+    $mv = $mv & ~0x80000000;
+    $mv = -2147483648 + $mv;
+  }
+  return $mv;
+}
 
+function assignmentToMem($params, $memory) {
+  $memVar = $params[0];
+  if(count($params) === 3) {
+    $rval = $params[2];
+    if(is_numeric($rval)) {
+      // Var-to-num assignment
+      $rval = intval($rval);
+    } else {
+      // Var-to-var assignment
+      $rval = $memory[$rval];
+    }
+    $memory[$memVar] = $rval;
+  } else if(count($params) === 5) {
+    $val1 = $params[2];
+    $op = $params[3];
+    $val2 = $params[4];
+    if(is_numeric($val1)) {
+      $val1 = intval($val1);
+    } else {
+      $val1 = $memory[$val1];
+    }
+    if(is_numeric($val2)) {
+      $val2 = intval($val2);
+    } else {
+      $val2 = $memory[$val2];
+    }
+    switch($op) {
+    case '+':
+      $memory[$memVar] = $val1 + $val2;
+      break;
+    case '-':
+      $memory[$memVar] = $val1 - $val2;
+      break;
+    case '*':
+      $memory[$memVar] = $val1 * $val2;
+      break;
+    case '/':
+      $memory[$memVar] = $val1 / $val2;
+      break;
+    }
+    $memory[$memVar] = to32BitSigned((int)($memory[$memVar]));
+  }
+  return $memory;
+}
+
+
+//------------------------------------------------------------------------------
+// Read in BASIC instructions line-by-line.
 while(fscanf(STDIN, '%[^\n]', $in)){
   $inExpl = explode(' ', $in);
   $params = [];
+
+  // Skip first two elems because everything $i >= 2 are parameters for the
+  // command ($inExpl[1] or $ins->parameters()).
   for($i = 2; $i < count($inExpl); $i++) {
     $params[] = $inExpl[$i];
   }
   $instructions[] = new Instruction(intval($inExpl[0]), $inExpl[1], $params);
 }
 
+
+//------------------------------------------------------------------------------
+// Instructions may be passed in an arbitrary order; make order ready for
+// execution.
 usort($instructions, 'cmp');
 
-for($i = 0; $i < size($instructions); $i++) {
+
+//------------------------------------------------------------------------------
+// BASIC parser and interpreter
+
+// We run through the sorted instructions.
+// GOTOs are handled by simply setting $i to whatever ::line() corresponds (see
+// lineToArrayIndex()).
+for($i = 0; $i < count($instructions); $i++) {
   $ins = $instructions[$i];
-  // TODO implement parsing and executing of instructions
   switch($ins->command()) {
   case 'LET':
+    /*
+      POSSIBLE PARAMS
+      count($ins->parameters()) == 3 means simple assignment:
+        <VAR> = <NUM>
+        <VAR> = <VAR>
+      count($ins->parameters()) == 5 means assignment with arithmetic:
+        <VAR> = <NUM> {+,-,*,/} <NUM>
+        <VAR> = <VAR> {+,-,*,/} <NUM>
+        <VAR> = <NUM> {+,-,*,/} <VAR>
+        <VAR> = <VAR> {+,-,*,/} <VAR>
+     */
+    $memory = assignmentToMem($ins->parameters(), $memory);
     break;
+    //----------------------------------------------------------------------------
   case 'IF':
+    // TODO
     break;
+    //----------------------------------------------------------------------------
   case 'PRINT':
+    /* Possible params
+      "<STR>"
+     */
+    // TODO
     break;
+    //----------------------------------------------------------------------------
   case 'PRINTLN':
+    /* Possible params
+      "<STR>"
+     */
+    // TODO
     break;
+    //----------------------------------------------------------------------------
+    // default: noop, unrecognized command
   }
 }
+print_r($memory);
 
 ?>
